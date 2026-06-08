dd9b46d450cbc22259ef4dcdc4f413899b59bf56
chillichez_22 <172315193+chillichez-22@users.noreply.github.com>
2026-06-08 12:12:09 +0100

    Added SDL3 as a package

--- spicy-engine/source/main.cpp
+++ spicy-engine/source/main.cpp
@@ -2,10 +2,19 @@
 #include "shape.hpp"
 
 #include <iostream>
+#include <SDL3/SDL.h>
 
 int main() {
 
 	std::cout << "hello world" << "\n";
+
+	if ( !SDL_Init( SDL_INIT_VIDEO ) ){
+		std::cout << "SDL failed to link" << "\n";
+	}
+	else{
+		std::cout << "SDL linked succesfully" << "\n";
+	}
+
 	std::getchar();
 	return 0;
 }
