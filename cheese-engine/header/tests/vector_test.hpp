effe881f6498981e850e1c1ed9986584c7b4ba4e
chillichez_22 <172315193+chillichez-22@users.noreply.github.com>
2026-07-12 14:15:31 +0100

    Updated Main Testing Scene and Vector Testing

--- cheese-engine/header/tests/vector_test.hpp
+++ cheese-engine/header/tests/vector_test.hpp
@@ -6,33 +6,3 @@ bool areVectorsEqual( Vector2 vectorOne, Vector2 vectorTwo );
 bool areVectorsEqual( Vector3 vectorOne, Vector3 vectorTwo );
 bool areVectorsEqual( Vector4 vectorOne, Vector4 vectorTwo );
 
-
-// Vector2 
-
-/**
- * @brief Tests the addition of two Vector2s
- * 
- * @return If the test was successful
- */
-bool testVector2Adding();
-
-/**
- * @brief Tests the subtraction of two Vector2s
- * 
- * @return If the test was successful
- */
-bool testVector2Subbing();
-
-/**
- * @brief Tests the multiplication of two Vector2s
- * 
- * @return If the test was successful
- */
-bool testVector2Multiplying();
-
-/**
- * @brief Tests the division of two Vector2s
- * 
- * @return If the test was successful
- */
-bool testVector2Dividing();
--- cheese-engine/source/testing.cpp
+++ cheese-engine/source/testing.cpp
@@ -9,56 +9,13 @@
 #include <SDL3/SDL.h>
 
 
-// Testing
-
-int HandleTest( bool testCase ){
-
-	if ( testCase ){
-		std::cout << "PASSED \n";
-	}
-	else{
-		std::cout << "FAILED \n";
-		throw std::logic_error("TEST FAILED");
-	}
-
-	return 0;
-}
-
-int TestVector2(){
-
-	std::cout << "test Vector2 Adding ";
-	HandleTest( testVector2Adding() );
-
-	std::cout << "test Vector2 Subbing ";
-	HandleTest( testVector2Subbing() );
-
-	std::cout << "test Vector2 Multiplying ";
-	HandleTest( testVector2Multiplying() );
-
-	std::cout << "test Vector2 Division ";
-	HandleTest( testVector2Dividing() );
-
-	return 0;
-}
-
-
-int Testing(){
-
-	std::cout << "Testing Started \n\n";
-
-	TestVector2();
-
-	std::cout << "Testing Ended \n";
-	return 0;
-}
-
 int main() {
 
-	std::cout << "Testing Loaded \n";
-	std::cout << "CPP version: " << __cplusplus << "\n";
-
-	Testing();
+	std::cout << "Testing Scene Loaded.";
 
 	std::getchar(); 
+
+	std::cout << "Testing Scene Ended.";
+	
 	return 0;
 }
